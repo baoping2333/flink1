51b16b79559f8d8e8a4af50fcf0156415ab0c9bd
bpw <1278299804@qq.com>
2022-04-06 15:52:23 +0800

    sream数据处理

--- src/main/java/com/ping/flink01/StreamWordCount1.java
+++ src/main/java/com/ping/flink01/StreamWordCount1.java
@@ -25,18 +25,17 @@ public class StreamWordCount1 {
                 7777);
         // 3. 转换数据格式
         SingleOutputStreamOperator<Tuple2<String, Long>> wordAndOne = lineDSS
-                .flatMap((String line, Collector<String> words) -> {
-                    Arrays.stream(line.split(" ")).forEach(words::collect);
+                .flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+                    String[] words = line.split(" ");
+                    for (String word : words) {
+                        out.collect(Tuple2.of(word, 1L));
+                    }
                 })
-                .returns(Types.STRING)
-                .map(word -> Tuple2.of(word, 1L))
                 .returns(Types.TUPLE(Types.STRING, Types.LONG));
         // 4. 分组
-        KeyedStream<Tuple2<String, Long>, String> wordAndOneKS = wordAndOne
-                .keyBy(t -> t.f0);
+        KeyedStream<Tuple2<String, Long>, String> wordAndOneKS = wordAndOne.keyBy(t -> t.f0);
         // 5. 求和
-        SingleOutputStreamOperator<Tuple2<String, Long>> result = wordAndOneKS
-                .sum(1);
+        SingleOutputStreamOperator<Tuple2<String, Long>> result = wordAndOneKS.sum(1);
         // 6. 打印
         result.print();
         // 7. 执行
